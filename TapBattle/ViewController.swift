7c0458933951a3c0c4d91cce5c96906953a05e22
Doni Ramadhan <doniramadhan@gmail.com>
2016-12-12 01:02:40 +0100

    first prototype simply tap on either red or blue area to increase the area size itself

--- TapBattle/ViewController.swift
+++ TapBattle/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    
+    var player1NumberOfTap = 1
+    var player2NumberOfTap = 1
+    
+    @IBAction func player1DidTap(_ sender: Any) {
+        player1NumberOfTap += 1
+        updateHeight()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func player2DidTap(_ sender: Any) {
+        player2NumberOfTap += 1
+        updateHeight()
     }
-
-
+    
+    func updateHeight(){
+        let height =  CGFloat(player1NumberOfTap - player2NumberOfTap)*10
+        heightConstraint.constant = height
+        view.layoutIfNeeded()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
